86c01fd31d53e47c135e71797b899baa5e3f4739
DetectiveQuincy <55505883+DetectiveQuincy@users.noreply.github.com>
2019-10-29 10:25:06 -0600

    Add files via upload

---
+++ Other/basic_print.py
@@ -0,0 +1,21 @@
+#--------------------------------------------------
+# Name:        Import Assignment
+# Purpose:
+#
+# Author:      kenny.coons
+# From : Other > basic_print
+# Created:     28/10/2019
+# Copyright:   (c) kenny.coons 2019
+# Licence:     <your licence>
+#-------------------------------------------------
+
+#----------------Lists and Dictionaries----------
+dope_list = ["potato", "yum", "good food"]
+
+#---------------Functions-----------------------
+def cool_function():
+    add_list = input("What would you like to add to this list?")
+    dope_list.append(add_list)
+
+
+arthur = "cowpoke"
