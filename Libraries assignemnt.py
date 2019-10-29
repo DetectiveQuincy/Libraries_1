796dcea195b46179a795762c094359b5d199e0ef
DetectiveQuincy <55505883+DetectiveQuincy@users.noreply.github.com>
2019-10-29 10:24:06 -0600

    Add files via upload

---
+++ Libraries assignemnt.py
@@ -0,0 +1,23 @@
+#-------------------------------------------------------------------------------
+# Name:        Import Assignment
+# Purpose:
+#
+# Author:      kenny.coons
+#
+# Created:     28/10/2019
+# Copyright:   (c) kenny.coons 2019
+# Licence:     <your licence>
+#-------------------------------------------------------------------------------
+#Imports
+import time
+
+from Other import basic_print
+from Other import more_stuff
+
+#Functions
+
+#Main
+print (basic_print.arthur)
+basic_print.cool_function()
+print (basic_print.dope_list)
+print(more_stuff.breakfast_dictionary)
